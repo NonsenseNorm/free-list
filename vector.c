9c9983585c94267ccab38e3579f6415965405b95
Tanizak Soma <xndu09@gmail.com>
2025-06-18 08:11:30 +0900

    Vector構造体に関数ポインタとする，実装はすべてstatic関数にすることで他のライブラリとの衝突を避けられる

--- vector.c
+++ vector.c
@@ -1,9 +1,47 @@
 #include "vector.h"
 
-int	get_element(const Vector* vector, size_t i)
+static int	get_element(const Vector* vector, size_t i)
 {
 	return 0;
 }
+
+
+static STATUS	resize(Vector*	vector)
+{
+	STAUTS	status;
+	Block*	current;
+	Block*	new_block;
+
+	current = vector->sentinel.next;
+	while(current != sentinel)
+		current = current->next;
+	
+	new_block = malloc(sizeof(Block));
+	if (!new_block)
+		reuturn FAILD;
+	current->next = new_block;
+	new_block->next = sentinel;
+	new_block->prev = current;
+
+	vector->capacity += CAPACITY;
+
+staitc STATUS	append(Vector* vector, int data)
+{
+	STATUS	status;
+
+	if (vector->used >= vector->capacity) {
+		status = resize(vector);
+		if (status == SUCCEED)
+		{
+			
+		}
+	block = malloc(sizeof(Block));
+	if (!block)
+		return (status = FAILD, status);
+
+	
+static STATUS	pop(Vector* vector)
+{
 
 Vector*	init_vector()
 {
