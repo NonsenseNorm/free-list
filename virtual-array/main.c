0539a0aa11422eeee5bdf7ced292d6340e81b22d
mizuki <xndu09@gmail.com>
2025-06-26 20:25:25 +0900

    saving: バグるべきプログラムが全くバグらない

--- virtual-array/main.c
+++ virtual-array/main.c
@@ -5,23 +5,18 @@ int	main()
 	VirtualArray* v;
 
 	/*sizeof(char)*10でばぐる*/
-	v = create_virtual_array(1);
+	v = create_virtual_array(2);
 	if (v == NULL)
 		return 1;
 
-	size_t i = 0;
-	while (i < 100 && virtual_array(v, i) != NULL)
+	int i = 0;
+	while (i < 100)
 	{
-		*virtual_array(v, i) = (int)i;
+		*((int *)virtual_array(v, i)) = i*10000;
+		printf("varray[%d]: %d\n", i, *((int *)virtual_array(v, i)));
 		i++;
 	}
 
-	size_t n = 0;
-	while (n < 100)
-	{
-		printf("v_array[%ld]: %d\n", n, (int)*virtual_array(v, n));
-		n++;
-	}
 	dispose_virtual_array(v);
 	return 0;
 }
